9291a82c24a8516d5bb0fae651eada8a555ceaa9
gokul <gokulhansv@gmail.com>
2023-10-26 23:08:23 +0530

    baseURL updated

--- frontend/src/axios.jsx
+++ frontend/src/axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.API_BASE_URL || 'http://localhost:5000/api';
+const baseURL = import.meta.env.API_BASE_URL;
 
 const axiosClient = axios.create({
     baseURL: baseURL,
