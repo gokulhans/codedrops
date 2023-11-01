99378da9d39faf5781efeb5a4d1854402e99da90
gokul <gokulhansv@gmail.com>
2023-11-01 09:52:31 +0530

    vercel analytics added, browser router added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import('preline')
 import './index.css'
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Analytics } from '@vercel/analytics/react';
 
 const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+        <Analytics />
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 )
