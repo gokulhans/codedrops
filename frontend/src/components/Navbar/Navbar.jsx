4cd56895dd88537505bdaa10db74a09c98243881
gokul <gokulhansv@gmail.com>
2023-11-01 14:15:57 +0530

    navbar updated

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -65,13 +65,13 @@ const Navbar = ({ isAuth, setIsAuth }) => {
             className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block"
           >
             <div className="flex flex-col gap-y-4 p-2 md:p-0 gap-x-0 mt-5 md:flex-row md:items-center md:justify-end md:gap-y-0 md:gap-x-7 md:mt-0 md:pl-7">
-              <a
+              <Link
                 className="font-medium text-blue-600 md:py-6 dark:text-blue-500"
-                href="#"
+                to={"/"}
                 aria-current="page"
               >
                 Home
-              </a>
+              </Link>
               <Link to={"/drops"}
                 className="font-medium text-gray-500 hover:text-gray-400 md:py-6 dark:text-gray-400 dark:hover:text-gray-500"
               >
